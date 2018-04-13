3864349a64e04f6a846040b8a6464bad2998ca94
Kamil <bizkamiladamski@gmail.com>
2018-04-13 18:39:30 +0000

    working version friday night

--- js/renderFrame.js
+++ js/renderFrame.js
@@ -18,60 +18,65 @@ function deleteInput(e){
 function Frame(question="",type="Yes/No",condition1,condition2){
     this.question=question;
     this.type=type;
-    this.condition1=condition1;
+    this.isSubquestion=condition1;
     this.condition2=condition2;
     
     Frame.prototype.createIt= function(cont){
         
         
         const patter=`
-          <li class="list-group-item border-0">
-            <div class="card border-dark frame-body-element">
-              <div class="card-body text-dark">
-                <form name="homeform">
-                  
-                  
-                <div class="form-group row">
-                  <label class="col-sm-2 col-form-label" for="conditions">Question</label>
-                  <div class="col-sm-6">
-                    <select id="conditions" class="form-control">
-                      <option>Equals</option>
-                      <option>Greather than</option>
-                      <option>Less than</option>
-                    </select>
-                  </div>
-                  <div class="col-sm-4">
-                    <select id="conditions" class="form-control">
-                      <option>Yes</option>
-                      <option>No</option>
-                    </select>
-                  </div>
-                </div>
-                <div class="form-group row">
-                  <label class="col-sm-2 col-form-label" for="inputQuestion">Question</label>
-                  <div class="col-sm-10">
-                    <input type="text" class="form-control" id="inputQuestion">
-                  </div>
-                </div>
-                <div class="form-group row">
-                  <label for="inputType" class="col-sm-2 col-form-label">Type</label>
-                  <div class="col-sm-10">
-                    <select id="inputType" class="form-control">
-                      <option>Yes/No</option>
-                      <option>Text</option>
-                      <option>Number</option>
-                    </select>
+        <ul id="container-list" class="list-group border-0">
+            <li class="list-group-item border-0">
+                <div class="card border-dark frame-body-element">
+                  <div class="card-body text-dark">
+                    <form name="homeform">
+                      
+                      
+                    <div class="form-group row">
+                      <label class="col-sm-2 col-form-label" for="conditions">Question</label>
+                      <div class="col-sm-6">
+                        <select id="conditions" class="form-control">
+                          <option>Equals</option>
+                          <option>Greather than</option>
+                          <option>Less than</option>
+                        </select>
+                      </div>
+                      <div class="col-sm-4">
+                        <select id="conditions" class="form-control">
+                          <option>Yes</option>
+                          <option>No</option>
+                        </select>
+                      </div>
                     </div>
+                    <div class="form-group row">
+                      <label class="col-sm-2 col-form-label" for="inputQuestion">Question</label>
+                      <div class="col-sm-10">
+                        <input type="text" class="form-control" id="inputQuestion">
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label for="inputType" class="col-sm-2 col-form-label">Type</label>
+                      <div class="col-sm-10">
+                        <select id="inputType" class="form-control">
+                          <option>Yes/No</option>
+                          <option>Text</option>
+                          <option>Number</option>
+                        </select>
+                        </div>
+                    </div>
+                    <div class="float-right">
+                      <a href="#" class="btn btn-sm btn-dark addSubInput">Add-Sub-Input</a>
+                      <a href="#" class="btn btn-sm btn-dark deleteInput">Delete</a>
+                    </div>
+                    </form>
+                  </div>
                 </div>
-                <div class="float-right">
-                  <a href="#" class="btn btn-sm btn-dark addSubInput">Add-Sub-Input</a>
-                  <a href="#" class="btn btn-sm btn-dark deleteInput">Delete</a>
-                </div>
-                </form>
-              </div>
-            </div>
-          </li>
+             </li>
+        </ul>
         `;
+        
+        
+          
         
         
         
