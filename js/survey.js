8d45eda364daee1916d221e2e912d60693a167db
Kamil <bizkamiladamski@gmail.com>
2018-04-18 13:40:52 +0000

    changed pattern for preview due to same parent/child structure

--- js/survey.js
+++ js/survey.js
@@ -1,41 +1,66 @@
 function previewSurvey(){
 const containerList=document.querySelector("#container-list");
-    
-
 project.forEach(function(question){
-let pattern;
-const ulToAdd=document.createElement("ul");
-ulToAdd.className="preview text-left";
-
-      if(question.type=="Yes/No"){
-        pattern=`
-        ${question.question}
+  let pattern;
+  const ulToAdd=document.createElement("ul");
+  ulToAdd.className="preview text-left";
+  
+        if(question.type=="Yes/No"){
+          pattern=`
+          
+              <li class="preview d-block">
+              <div class="questionAsked">${question.question}</div>
+                  <div class="form-check form-check-inline">
+                      <input class="form-check-input" type="radio" name="previewRadio" id="previewRadioYes" value="Yes">
+                      <label class="form-check-label" for="previewRadio">
+                          Yes
+                      </label>
+                  </div>
+                  
+                  <div class="form-check form-check-inline">
+                      <input class="form-check-input" type="radio" name="previewRadio" id="previewRadioNo" value="No">
+                      <label class="form-check-label" for="previewRadio">
+                          No
+                      </label>
+                  </div>
+            </li>`;    
+          
+        }else{
+          pattern=`
             <li class="preview">
-                <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1">
-                    <label class="form-check-label" for="exampleRadios1">
-                        Yes
-                    </label>
-                </div>
-                
-                <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
-                    <label class="form-check-label" for="exampleRadios2">
-                        No
-                    </label>
-                </div>
-          </li>`;    
-        
-      }else{
-        pattern=`
-          ${question.question}
-            <li class="preview">
-              <input type="text" class="form-control mb-2 mr-sm-2" id="inlineFormInputName2">
-          </li>`;
-      }
-      ulToAdd.innerHTML=pattern;
-      containerList.appendChild(ulToAdd);
-});
-    
-    
+            <div class="questionAsked">${question.question}</div>
+              <div>
+                <input type="text" class="form-control mb-2 mr-sm-2" id="previewInput">
+              </div>
+            </li>`;
+        }
+        ulToAdd.innerHTML=pattern;
+        containerList.appendChild(ulToAdd);
+  });
 }
+
+
+
+
+function hasChangeInPreview(e){
+  const changedElementUl=e.target.parentElement.parentElement.parentElement;
+  const placeToPasteNewElement=e.target.parentElement.parentElement;
+  const changedValue=e.target.value;
+  dbSequence=[];
+  buildSequence(changedElementUl);
+  
+  console.log(changedElementUl);
+  console.log(placeToPasteNewElement);
+  console.log(changedValue);
+  
+}
+
+
+
+
+
+
+
+
+
+
